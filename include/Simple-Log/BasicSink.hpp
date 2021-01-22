755ab77e0d93c9c896458ee3a254cea2f0954699
DNKpp <DNKpp2011@gmail.com>
2021-01-22 19:19:40 +0100

    apply filter and formatting system on BasicSink
    Signed-off-by: DNKpp <DNKpp2011@gmail.com>

--- include/Simple-Log/BasicSink.hpp
+++ include/Simple-Log/BasicSink.hpp
@@ -8,19 +8,42 @@
 
 #pragma once
 
+#include <functional>
 #include <ostream>
+#include <string>
+#include <mutex>
 
+#include "Concepts.hpp"
 #include "ISink.hpp"
 
 namespace sl::log
 {
+	using Formatter_t = std::function<void(std::ostream&, const Record&)>;
+	using Filter_t = std::function<bool(const Record&)>;
+
 	class BasicSink :
 		public ISink
 	{
+	protected:
+		static auto defaultFormatter() noexcept
+		{
+			return [](std::ostream& out, const Record& rec)
+			{
+				out << rec.message;
+			};
+		}
+
+		static auto defaultFilter() noexcept
+		{
+			return [](const Record& rec) { return true; };
+		}
+
 	public:
 		BasicSink(std::ostream& stream) :
 			m_Stream{ stream }
 		{
+			m_Formatter = defaultFormatter();
+			m_Filter = defaultFilter();
 		}
 
 		~BasicSink() noexcept = default;
@@ -30,14 +53,50 @@ namespace sl::log
 
 		BasicSink(BasicSink&&) = delete;
 		BasicSink& operator =(BasicSink&&) = delete;
-		
+
 		void log(const Record& record) override
 		{
-			m_Stream << record.message << std::endl;
+			std::scoped_lock lock{ m_FilterMx, m_FormatterMx };
+			if (m_Filter(record))
+			{
+				m_Formatter(m_Stream, record);
+				m_Stream << std::endl;
+			}
+		}
+
+		template <RecordFormatter TFormatter>
+		void setFormatter(TFormatter&& formatter) noexcept
+		{
+			std::scoped_lock lock{ m_FormatterMx };
+			m_Formatter = std::forward<TFormatter>(formatter);
+		}
+
+		void removeFormatter() noexcept
+		{
+			std::scoped_lock lock{ m_FormatterMx };
+			m_Formatter = defaultFormatter();
+		}
+
+		template <RecordFilter TFilter>
+		void setFilter(TFilter&& filter) noexcept
+		{
+			std::scoped_lock lock{ m_FilterMx };
+			m_Filter = std::forward<TFilter>(filter);
+		}
+
+		void removeFilter() noexcept
+		{
+			std::scoped_lock lock{ m_FilterMx };
+			m_Filter = defaultFilter();
 		}
 
 	private:
 		std::ostream& m_Stream;
+		
+		std::mutex m_FormatterMx;
+		Formatter_t m_Formatter;
+		std::mutex m_FilterMx;
+		Filter_t m_Filter;
 	};
 }
 
