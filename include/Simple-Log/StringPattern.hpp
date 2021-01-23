a2d18dd21460861d52142806b207090150e47e57
DNKpp <DNKpp2011@gmail.com>
2021-01-23 23:36:29 +0100

    cleanup StringPattern
    Signed-off-by: DNKpp <DNKpp2011@gmail.com>

--- include/Simple-Log/StringPattern.hpp
+++ include/Simple-Log/StringPattern.hpp
@@ -9,11 +9,11 @@
 #pragma once
 
 #include <algorithm>
+#include <charconv>
 #include <chrono>
 #include <functional>
 #include <iomanip>
 #include <sstream>
-#include <charconv>
 
 namespace sl::log::detail
 {
@@ -32,9 +32,9 @@ namespace sl::log::detail
 	struct DateTimeGenerator
 	{
 		std::string token;
+
 		void operator()(std::ostream& stream) const
 		{
-			using namespace std::literals;
 			const auto now = std::chrono::system_clock::now();
 			auto t_c = std::chrono::system_clock::to_time_t(now);
 			stream << std::put_time(std::localtime(&t_c), token.c_str());
@@ -44,6 +44,7 @@ namespace sl::log::detail
 	struct StringGenerator
 	{
 		std::string_view str;
+
 		void operator()(std::ostream& stream) const
 		{
 			stream << str;
@@ -61,7 +62,7 @@ namespace sl::log
 			m_TokenGenerators{ makeTokenGeneratorsFromPatternString() }
 		{
 		}
-		
+
 		std::string next()
 		{
 			std::ostringstream ss;
@@ -98,30 +99,34 @@ namespace sl::log
 
 				switch (*(tokenBegin + 1))
 				{
-				case 'H':	// hour (2 digits)
-				case 'M':	// minute (2 digits)
-				case 'S':	// second (2 digits)
+				case 'H': // hour (2 digits)
+				case 'M': // minute (2 digits)
+				case 'S': // second (2 digits)
 
-				case 'Y':	// year (4 digits)
-				case 'm':	// month (2 digits)
-				case 'd':	// day of month
-				case 'j':	// day of year
+				case 'Y': // year (4 digits)
+				case 'm': // month (2 digits)
+				case 'd': // day of month
+				case 'j': // day of year
 					generators.emplace_back(detail::DateTimeGenerator{ { tokenBegin, itr + 1 } });
 					itr = tokenBegin + 2;
 					break;
 
-				case 'N':	// IncNumber
+				case 'N': // IncNumber
 					generators.emplace_back(detail::IncNumberGenerator{ .minWidth = 0 });
 					itr = tokenBegin + 2;
 					break;
-				default:	// IncNumber with custom width
+				default: // IncNumber with custom width
 					auto tokenEnd = std::ranges::find(tokenBegin + 1, end, 'N');
 					// from_chars perform conversions until the first not convertible sign is detected, but in this case it should fail when there is any non-convertible sign
 					// between %...N, thus I'll pre-check it here
-					if (tokenEnd != end && std::ranges::all_of(tokenBegin + 1, tokenEnd, [](char c) { return '0' <= c && c <= '9'; }))
+					if (tokenEnd != end && std::ranges::all_of(
+																tokenBegin + 1,
+																tokenEnd,
+																[](char c) { return '0' <= c && c <= '9'; }
+															))
 					{
 						std::size_t width = 0;
-						if(auto [p, ec] = std::from_chars(&*(tokenBegin + 1), &*tokenEnd, width); ec == std::errc())
+						if (auto [p, ec] = std::from_chars(&*(tokenBegin + 1), &*tokenEnd, width); ec == std::errc())
 						{
 							generators.emplace_back(detail::IncNumberGenerator{ .minWidth = width });
 							itr = tokenEnd + 1;
