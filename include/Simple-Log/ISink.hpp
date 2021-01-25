d98e873b4a600fd08615896aa7441786e1326693
DNKpp <DNKpp2011@gmail.com>
2021-01-25 20:50:34 +0100

    add doxygen documentation in ISink.hpp
    Signed-off-by: DNKpp <DNKpp2011@gmail.com>

--- include/Simple-Log/ISink.hpp
+++ include/Simple-Log/ISink.hpp
@@ -11,14 +11,32 @@
 #include "Record.hpp"
 
 namespace sl::log
-{	
+{
+	/** \addtogroup sink
+	 * @{
+	 */
+
+	/**
+	 * \brief Sink interface class
+	 * \details The interface class for each derived Sink type. If you want to implement your custom Sink type, have a look at BasicSink first. This might be a better starting point for customizations.
+	 */
 	class ISink
 	{
 	public:
+		/**
+		 * \brief virtual default destructor
+		 */
 		virtual ~ISink() noexcept = default;
 
+		/**
+		 * \brief virtual log function
+		 * \details Will be called from the Worker thread.
+		 * \param record The record about to be processed by the sink
+		 */
 		virtual void log(const Record& record) = 0;
 	};
+
+	/** @}*/
 }
 
 #endif
