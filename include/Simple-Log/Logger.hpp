1eab767745a545cd5d777ac89d307c281a930b2e
DNKpp <DNKpp2011@gmail.com>
2021-01-29 00:37:23 +0100

    removes log function from Logger; add doxygen documentation for Logger.hpp
    Signed-off-by: DNKpp <DNKpp2011@gmail.com>

--- include/Simple-Log/Logger.hpp
+++ include/Simple-Log/Logger.hpp
@@ -22,9 +22,22 @@
 
 namespace sl::log
 {
+	/**
+	 * \brief Convenience class for generating Record s
+	 * \details This class is in fact a template for upcoming Record s. It stores default settings (e.g. severity level and channel) for newly generated Record s. Using its operator () users can start building Record s
+	 * in an effective and elegant manner.
+	 */
 	class Logger
 	{
 	public:
+		/**
+		 * \brief Constructor
+		 * \details Creates a Logger instance and links it to the specified core instance
+		 * \param core Specific core instance
+		 * \param defaultSeverityLvl Default severity level for generated Records.
+		 * \param defaultChannel Default channel for generated Records.
+		 * \param defaultUserData Default user data for generated Records.
+		 */
 		Logger(
 			Core& core,
 			std::any defaultSeverityLvl = {},
@@ -38,28 +51,43 @@ namespace sl::log
 		{
 		}
 
+		/**
+		 * \brief Destructor
+		 */
 		~Logger() noexcept = default;
 
+		/**
+		 * \brief Copy constructor
+		 */
 		Logger(const Logger&) noexcept = default;
+		/**
+		 * \brief Copy assign operator
+		 */
 		Logger& operator =(const Logger&) noexcept = default;
+
+		/**
+		 * \brief Move constructor
+		 */
 		Logger(Logger&&) noexcept = default;
+		/**
+		 * \brief Move assign operator
+		 */
 		Logger& operator =(Logger&&) noexcept = default;
 
-		// ReSharper disable once CppMemberFunctionMayBeConst
-		// Logger should semantically not be const, when writing to the logs
-		void log(Record rec)
-		{
-			assert(m_Core);
-			m_Core->log(std::move(rec));
-		}
-
+		/**
+		 * \brief Creates a new instance of RecordBuilder
+		 * \details This is the entry point for producing new Record s. The returned RecordBuilder object will be pre-initialized with each default values,
+		 * which might be overriden during the Record building process. 
+		 * \return Newly created RecordBuilder instance.
+		 */
 #ifdef __cpp_lib_source_location
 		RecordBuilder operator ()(const std::source_location& srcLoc = std::source_location::current())
 #else
 		RecordBuilder operator ()()
 #endif
 		{
-			RecordBuilder builder{ [&logger = *this](Record rec) { logger.log(std::move(rec)); } };
+			assert(m_Core);
+			RecordBuilder builder{ [&core = *m_Core](Record rec) { core.log(std::move(rec)); } };
 			builder.record().severity = m_DefaultSeverityLvl;
 			builder.record().channel = m_DefaultChannel;
 			builder.record().userData = m_DefaultUserData;
@@ -69,6 +97,60 @@ namespace sl::log
 #endif
 
 			return builder;
+		}
+
+		/**
+		 * \brief Setter for the default severity level
+		 * \param sevLvl New default value
+		 */
+		void setDefaultSeverityLevel(std::any sevLvl) noexcept
+		{
+			m_DefaultSeverityLvl = std::move(sevLvl);
+		}
+
+		/**
+		 * \brief Getter for the default severity level
+		 * \return Returns a const reference to the default severity level.
+		 */
+		[[nodiscard]] const std::any& defaultSeverityLevel() const noexcept
+		{
+			return m_DefaultSeverityLvl;
+		}
+
+		/**
+		 * \brief Setter for the default channel
+		 * \param channel New default value
+		 */
+		void setDefaultChannel(std::any channel) noexcept
+		{
+			m_DefaultChannel = std::move(channel);
+		}
+
+		/**
+		 * \brief Getter for the default channel
+		 * \return Returns a const reference to the default channel.
+		 */
+		[[nodiscard]] const std::any& defaultChannel() const noexcept
+		{
+			return m_DefaultChannel;
+		}
+
+		/**
+		 * \brief Setter for the default user data
+		 * \param userData New default value
+		 */
+		void setDefaultUserData(std::any userData) noexcept
+		{
+			m_DefaultUserData = std::move(userData);
+		}
+
+		/**
+		 * \brief Getter for the default user data
+		 * \return Returns a const reference to the default user data.
+		 */
+		[[nodiscard]] const std::any& defaultUserData() const noexcept
+		{
+			return m_DefaultUserData;
 		}
 
 	private:
