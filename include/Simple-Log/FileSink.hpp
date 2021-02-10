dd2fb3ee8117d38ea0227a9cd5e3b3ceddbc08af
DNKpp <DNKpp2011@gmail.com>
2021-02-11 00:44:02 +0100

    add doxygen documentation in FileSink.hpp
    Signed-off-by: DNKpp <DNKpp2011@gmail.com>

--- include/Simple-Log/FileSink.hpp
+++ include/Simple-Log/FileSink.hpp
@@ -23,24 +23,73 @@ namespace sl::log
 	 * @{
 	 */
 	
+	/**
+	 * \brief Class for logging into files 
+	 * \details Instances of this class are linked to a specific file and writes every Record into it. Users can setup \ref Rotation "rotation" and cleanup rules.
+	 *
+	 * \section FileNamePattern File name pattern
+	 * Users can provide a pattern string, which will be used for newly generated file names. Those file names will be generated when a new file shall be opened.
+	 * There are mainly two parts of those strings:
+	 * \li Constant parts are all substrings which do not contain any known tokens and will simply be used as is in file names.
+	 * \li Dynamic parts are substrings which contain know tokens, which will be replaced with actual data when a new file name will be generated. Look at the \ref Tokens "token" section of this page for further details.
+	 *
+	 * \subsection Tokens
+	 * Each token begins with \c %
+	 * Token	| Effect
+	 * ---------|--------
+	 * \%Y		| year in 4 digits
+	 * \%m		| month in 2 digits (including leading zeros)
+	 * \%d		| day of month in 2 digits (including leading zeros)
+	 * \%j		| day of year
+	 * \%H		| hour in 24h format (2 digits with leading zeros)
+	 * \%M		| minute (2 digits with leading zeros)
+	 * \%S		| second (2 digits with leading zeros)
+	 * \%N		| incrementing number starting at 1
+	 * \%xN		| incrementing number with x digits and leading zeros. Replace \a x with an actual number (might be multiple digits). Starts at 1.
+	 *
+	 * \section Rotation Rotation
+	 * Rotation rules are used as an indicator when to switch to a new file. Currently are two basic rules implemented:
+	 * \li file size
+	 * \li duration
+	 *
+	 * Each rule will be checked before a new Record will be written to file. If a new file shall be opened, a new file name will be generated via provided file name pattern.
+	 * If this file already exists, it will be overriden (or an exception raises if it is already in use).
+	 *
+	 * \section Cleanup Cleanup
+	 * While \ref Rotation "rotation" is used for indicating when to switch files, CleanupRule s are used for determining when to delete old files. Cleanup process will watch at the currently provided directory
+	 * and will only observe files with equally file extensions as provided in the file name pattern. If no extension (e.g. \c .abcd) is used, all files without an extension will be observed.
+	 */
+
 	class FileSink :
 		public BasicSink
 	{
 		using Super = BasicSink;
 
 	public:
+		/**
+		 * \brief Type for configuring FileSink rotation rules
+		 */
 		struct RotationRule
 		{
 			std::optional<std::size_t> fileSize;
 			std::optional<std::chrono::seconds> duration;
 		};
 
+		/**
+		 * \brief Type for configuring FileSink cleanup rules
+		 */
 		struct CleanupRule
 		{
 			std::optional<std::size_t> directorySize;
 			std::optional<std::size_t> fileCount;
 		};
 
+		/**
+		 * \brief Constructor
+		 * \details Constructs a new FileSink instance, which uses the provided file name pattern for newly opened files at the specified directory.
+		 * \param fileNamePattern	Pattern string from which new file names will be generated.
+		 * \param directory			The directory where all files of this sink will be generated.
+		 */
 		FileSink(std::string fileNamePattern, std::filesystem::path directory = std::filesystem::current_path()) :
 			BasicSink{ m_FileStream },
 			m_FileNamePattern{ std::move(fileNamePattern) },
@@ -48,6 +97,10 @@ namespace sl::log
 		{
 		}
 
+		/**
+		 * \brief Destructor
+		 * \details Finalizes and closes the current opened file (if any).
+		 */
 		~FileSink() noexcept
 		{
 			try
@@ -59,33 +112,64 @@ namespace sl::log
 			}
 		}
 
+		/**
+		 * \brief Deleted copy constructor
+		 */
 		FileSink(const FileSink&) = delete;
+		/**
+		 * \brief Deleted copy assign operator
+		 */
 		FileSink& operator =(const FileSink&) = delete;
+		/**
+		 * \brief Deleted move constructor
+		 */
 		FileSink(FileSink&&) = delete;
+		/**
+		 * \brief Deleted move assign operator
+		 */
 		FileSink& operator =(FileSink&&) = delete;
 
+		/**
+		 * \brief Applies a new RotationRule configuration.
+		 * \param rule The new RotationRule configuration.
+		 */
 		void setRotationRule(RotationRule rule) noexcept
 		{
 			std::scoped_lock lock{ m_RotationRuleMx };
 			m_RotationRule = rule;
 		}
 
+		/**
+		 * \brief Returns a copy of the current RotationRule configuration
+		 */
 		[[nodiscard]] RotationRule rotationRule() const noexcept
 		{
 			return load(m_RotationRule, m_RotationRuleMx);
 		}
 
+		/**
+		 * \brief Applies a new CleanupRule configuration.
+		 * \param rule The new CleanupRule configuration.
+		 */
 		void setCleanupRule(CleanupRule rule) noexcept
 		{
 			std::scoped_lock lock{ m_CleanupRuleMx };
 			m_CleanupRule = rule;
 		}
 
+		/**
+		 * \brief Returns a copy of the current CleanupRule configuration
+		 */
 		[[nodiscard]] CleanupRule cleanupRule() const noexcept
 		{
 			return load(m_CleanupRule, m_CleanupRuleMx);
 		}
 
+		/**
+		 * \brief Applies a new handler for opening files
+		 * \tparam THandler Type of handler (automatically deduced)
+		 * \param handler invokable handler object
+		 */
 		template <log::FileStateHandler THandler>
 		void setOpeningHandler(THandler&& handler) noexcept
 		{
@@ -93,12 +177,20 @@ namespace sl::log
 			m_OpeningHandler = std::forward<THandler>(handler);
 		}
 
+		/**
+		 * \brief Removes the active opening handler
+		 */
 		void removeOpeningHandler() noexcept
 		{
 			std::scoped_lock lock{ m_OpeningHandlerMx };
 			m_OpeningHandler = nullptr;
 		}
 
+		/**
+		 * \brief Applies a new handler for closing files
+		 * \tparam THandler Type of handler (automatically deduced)
+		 * \param handler invokable handler object
+		 */
 		template <log::FileStateHandler THandler>
 		void setClosingHandler(THandler&& handler) noexcept
 		{
@@ -106,13 +198,22 @@ namespace sl::log
 			m_ClosingHandler = std::forward<THandler>(handler);
 		}
 
+		/**
+		 * \brief Removes the active closing handler
+		 */
 		void removeClosingHandler() noexcept
 		{
 			std::scoped_lock lock{ m_ClosingHandlerMx };
 			m_ClosingHandler = nullptr;
 		}
 
-		void log(const Record& rec) override
+		/**
+		 * \brief Filters, formats and writes the passed record to the internal stream
+		 * \details This function prints the passed record to the internal file. In forehand \ref Rotation "rotation" will be checked if a new file shall be opened. Internally uses to the
+		 * BasicSink::log function.
+		 * \param record Record object
+		 */
+		void log(const Record& record) override
 		{
 			if (!m_FileStream.is_open())
 			{
@@ -124,7 +225,7 @@ namespace sl::log
 				openFile();
 			}
 
-			Super::log(rec);
+			Super::log(record);
 		}
 
 	private:
