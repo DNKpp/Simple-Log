5dcd93433a8a06866556cbf2af39e56ce8d38788
DNKpp <DNKpp2011@gmail.com>
2021-02-11 21:21:04 +0100

    add Simple-Log.hpp mass include header
    Signed-off-by: DNKpp <DNKpp2011@gmail.com>

---
+++ include/Simple-Log/Simple-Log.hpp
@@ -0,0 +1,27 @@
+//          Copyright Dominic Koepke 2021 - 2021.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          https://www.boost.org/LICENSE_1_0.txt)
+
+#ifndef SL_LOG_SIMPLE_LOG_HPP
+#define SL_LOG_SIMPLE_LOG_HPP
+
+#pragma once
+
+#include "Concepts.hpp"
+
+#include "StringPattern.hpp"
+
+#include "Record.hpp"
+#include "RecordBuilder.hpp"
+
+#include "RecordQueue.hpp"
+#include "Core.hpp"
+
+#include "Logger.hpp"
+
+#include "ISink.hpp"
+#include "BasicSink.hpp"
+#include "FileSink.hpp"
+
+#endif
