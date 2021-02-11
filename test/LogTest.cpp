cc1e56f03ce24cd0c4fc5d60090eb901fd7e2a83
DNKpp <DNKpp2011@gmail.com>
2021-02-11 12:26:40 +0100

    use cmake FetchContent instead of plain copy of catch2
    Signed-off-by: DNKpp <DNKpp2011@gmail.com>

--- test/LogTest.cpp
+++ test/LogTest.cpp
@@ -3,7 +3,7 @@
 //    (See accompanying file LICENSE_1_0.txt or copy at
 //          https://www.boost.org/LICENSE_1_0.txt)
 
-#include "catch.hpp"
+#include "catch2/catch.hpp"
 
 #include <iostream>
 
