db369ce520e0c716d4f6cbded0bb237a17f1e873
DNKpp <DNKpp2011@gmail.com>
2021-02-15 11:44:29 +0100

    add FilterTests.cpp
    Signed-off-by: DNKpp <DNKpp2011@gmail.com>

---
+++ src/tests/FilterTests.cpp
@@ -0,0 +1,15 @@
+//          Copyright Dominic Koepke 2021 - 2021.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          https://www.boost.org/LICENSE_1_0.txt)
+
+#include "catch2/catch.hpp"
+
+#include "Simple-Log/Filters.hpp"
+
+using namespace sl::log;
+
+SCENARIO("test interface correctness", "[filters]")
+{
+	
+}
