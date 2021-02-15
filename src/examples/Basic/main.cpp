e0281e10dfc50e0671e02444d7d2c3e3a54e2248
DNKpp <DNKpp2011@gmail.com>
2021-02-15 20:28:20 +0100

    update basic example
    Signed-off-by: DNKpp <DNKpp2011@gmail.com>

--- src/examples/Basic/main.cpp
+++ src/examples/Basic/main.cpp
@@ -3,13 +3,21 @@
 //    (See accompanying file LICENSE_1_0.txt or copy at
 //          https://www.boost.org/LICENSE_1_0.txt)
 
+// This is the header of preset types, thus you'll have a easy start getting used to the library.
 #include <Simple-Log/PresetTypes.hpp>
 
 #include <iostream>
 #include <memory>
 
+/* let's use a namepsace alias for convenience; be aware: if you use cmath, msvc will bloat your global namespace with a log function declaration.
+Thus to make it compatible with all compilers, I'll use logging as alias instead.
+
+All preset type alias are located in the sl::log::pre namespace, thus they do not interfere with the actual library if you don't want them to.
+It is no good style just importing everything into your namespace. Just create an namespace alias like so. This way it's very easy to make it less verbose for you.
+*/
 namespace logging = sl::log::pre;
 
+// We're using a factory function here, but this isn't necessary. You could also create a plain Core instance and set it up later in main
 inline std::unique_ptr<logging::Core_t> makeLoggingCore()
 {
 	/* an sl::log::Core is non-copy and non-movable, thus we will store it in an unique_ptr, so we
@@ -30,8 +38,24 @@ inline logging::Logger_t gLog{ *gLoggingCore, logging::SeverityLevel::info };
 int main()
 {
 	gLog() << "Hello, World!";	// This will print this message with the "info" severity
-	// creates a record with severity level "debug".
-	//gLog() << "Mighty debug message" << sl::log::SetSev{ sl::log::def::SeverityLevel::debug };
+	// override default logger settings at the beginning
+	gLog() << logging::SetSev::debug << "Mighty debug message";
+	// or at the end
+	gLog() << "Print my important hint!" << logging::SetSev::hint;
+	// or in between of messages
+	gLog() << "This" << " will " << logging::SetSev::warning << " create " << " a " << " concatenated " << " warning " << " message";
+	// and using default setup again
+	gLog() << "Back to info";
 }
 
 /*Core will make sure, that all pending Records will be processed before it gets destructed.*/
+/*
+ * The above code will for example generate this output:
+ *	18:49:32.047 >>> info:: Hello, World!
+ *	18:49:32.047 >>> debug:: Mighty debug message
+ *	18:49:32.047 >>> hint:: Print my important hint!
+ *	18:49:32.047 >>> warning:: This will  create  a  concatenated  warning  message
+ *	18:49:32.047 >>> info:: Back to info
+ *
+ * Keep in mind, you are completely free how you are going to format your message. This is just the default one.
+ */
