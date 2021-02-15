5272de0eac5a717ef710d4b251afc5b8a108cbb1
DNKpp <DNKpp2011@gmail.com>
2021-02-15 19:22:20 +0100

    cleanup and add missing docs for Record.hpp
    Signed-off-by: DNKpp <DNKpp2011@gmail.com>

--- include/Simple-Log/Record.hpp
+++ include/Simple-Log/Record.hpp
@@ -9,9 +9,7 @@
 #pragma once
 
 #include <chrono>
-#include <ostream>
 #include <string>
-#include <tuple>
 #include <version>
 
 #ifdef __cpp_lib_source_location
@@ -26,13 +24,18 @@ namespace sl::log
 
 	/**
 	 * \brief A collection of logging related information
-	 * \details This class serves as a simple collection of all the gathered information during a logging action.
+	 * \tparam TSeverityLevel Severity level type
+	 * \tparam TChannel Channel descriptor type
+	 * \tparam TMessage String type for storing messages
+	 * \tparam TTimePoint Type for storing time related information
+	 * 
+	 * \details This class serves as a simple collection of gathered information during a logging action. It is a basic implementation of the Record concept
+	 * and users are free to use this class as a starting point for creating expanded sub-classes or even create their own right from scratch. 
 	 */
 	template <std::semiregular TSeverityLevel,
-			std::semiregular TChannel,
-			std::semiregular TMessage = std::string,
-			std::semiregular TTimePoint = std::chrono::system_clock::time_point
-	>
+		std::semiregular TChannel,
+		std::semiregular TMessage = std::string,
+		std::semiregular TTimePoint = std::chrono::system_clock::time_point>
 	class BaseRecord
 	{
 	public:
@@ -44,48 +47,84 @@ namespace sl::log
 		using SourceLocation_t = std::source_location;
 #endif
 
+		/**
+		 * \brief Const access to the message string
+		 * \return Returns the stored message as std::string_view
+		 */
 		[[nodiscard]]
 		std::string_view message() const noexcept
 		{
 			return m_Message;
 		}
 
+		/**
+		 * \brief Sets the message string
+		 * \tparam UMessage Type which must be convertible to Message_t
+		 * \param msg Message content
+		 */
 		template <std::convertible_to<Message_t> UMessage>
 		void setMessage(UMessage&& msg)
 		{
 			m_Message = std::forward<UMessage>(msg);
 		}
 
+		/**
+		 * \brief Const access to the time stamp
+		 * \return Returns a const ref
+		 */
 		[[nodiscard]]
 		const TimePoint_t& timePoint() const noexcept
 		{
 			return m_TimePoint;
 		}
 
+		/**
+		 * \brief Sets the Record s time stamp
+		 * \tparam UTimePoint Type which must be convertible to TimePoint_t
+		 * \param timePoint A time point
+		 */
 		template <std::convertible_to<TimePoint_t> UTimePoint>
 		void setTimePoint(UTimePoint&& timePoint)
 		{
 			m_TimePoint = std::forward<UTimePoint>(timePoint);
 		}
 
+		/**
+		 * \brief Const access to the severity level
+		 * \return Returns a const ref
+		 */
 		[[nodiscard]]
 		const SeverityLevel_t& severity() const noexcept
 		{
 			return m_Severity;
 		}
 
+		/**
+		 * \brief Sets the Record s severity level
+		 * \tparam USeverityLevel Type which must be convertible to SeverityLevel_t
+		 * \param severity A time point
+		 */
 		template <std::convertible_to<SeverityLevel_t> USeverityLevel>
 		void setSeverity(USeverityLevel&& severity)
 		{
 			m_Severity = std::forward<USeverityLevel>(severity);
 		}
 
+		/**
+		 * \brief Const access to the channel
+		 * \return Returns a const ref
+		 */
 		[[nodiscard]]
 		const Channel_t& channel() const noexcept
 		{
 			return m_Channel;
 		}
 
+		/**
+		 * \brief Sets the Record s severity level
+		 * \tparam UChannel Type which must be convertible to Channel_t
+		 * \param channel channel descriptor
+		 */
 		template <std::convertible_to<Channel_t> UChannel>
 		void setChannel(UChannel&& channel)
 		{
@@ -93,12 +132,17 @@ namespace sl::log
 		}
 
 #ifdef __cpp_lib_source_location
+		/**
+		 * \brief Const access to the source location
+		 * \return Returns a const ref
+		 */
 		[[nodiscard]]
 		const SourceLocation_t& sourceLocation() const noexcept
 		{
 			return m_SourceLocation;
 		}
 
+		
 		template <std::convertible_to<SourceLocation_t> USourceLocation>
 		void setChannel(USourceLocation&& sourceLocation)
 		{
@@ -112,7 +156,7 @@ namespace sl::log
 		SeverityLevel_t m_Severity{};
 		Channel_t m_Channel{};
 #ifdef __cpp_lib_source_location
-		SourceLocation_t m_SourceLocation;
+		SourceLocation_t m_SourceLocation{};
 #endif
 	};
 
