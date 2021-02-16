516d11c3ee10a1f91664740fe75498c1a17c06cb
DNKpp <DNKpp2011@gmail.com>
2021-02-16 18:33:16 +0100

    add docs in Filters.hpp
    Signed-off-by: DNKpp <DNKpp2011@gmail.com>

--- include/Simple-Log/Filters.hpp
+++ include/Simple-Log/Filters.hpp
@@ -83,6 +83,22 @@ namespace sl::log::detail
 
 namespace sl::log
 {
+	/** \addtogroup filter
+	 * @{
+	 * \details Filters operate on Sink level and determine if a Record will be handled by that Sink object or skipped. Users are able apply provided filter like the ProjectionFilter types
+	 * or use custom types. Those custom types can be from any type which is invokable by the used Record type and returning a bool (or at least a bool-comparable-type) as result.
+	 *
+	 * There are already some convenience factory function present (e.g. makeMessageProjectionFor), which may help setting up filters quickly.
+	 */
+	
+	/**
+	 * \brief Combines a projection on Record type with a predicate into an invokable object
+	 * \tparam TProjection A Projection type, which has to accept an object of the used Record type and must return something
+	 * \tparam TUnaryPredicate A predicate type, which accepts objects returned by TProjection
+	 * \details Instances of this class are invokable with any types, which follow the Record concept. Due to possible custom implementations
+	 * or adjustments ProjectionFilter can't rely on the libraries BaseRecord type and therefore is not able to pre-check the provided Projection and
+	 * Predicate via concept.
+	 */
 	template <class TProjection, class TUnaryPredicate>
 	class ProjectionFilter
 	{
@@ -90,12 +106,23 @@ namespace sl::log
 		using Projection_t = TProjection;
 		using UnaryPredicate_t = TUnaryPredicate;
 
+		/**
+		 * \brief Constructor
+		 * \param projection Invokable object
+		 * \param predicate Predicate object
+		 */
 		constexpr ProjectionFilter(TProjection projection, TUnaryPredicate predicate) :
 			m_Projection(std::move(projection)),
 			m_Predicate(std::move(predicate))
 		{
 		}
 
+		/**
+		 * \brief Call-operator
+		 * \tparam TRecord Concrete type of the used Record object
+		 * \param rec Record object
+		 * \return Returns true, if the Record should be handled. False if skipped.
+		 */
 		template <Record TRecord>
 		constexpr bool operator ()(const TRecord& rec)
 		{
@@ -107,22 +134,43 @@ namespace sl::log
 		UnaryPredicate_t m_Predicate;
 	};
 
+	/**
+	 * \brief Chains multiple filter together
+	 * \tparam TAlgorithm The used algorithm, which will determine how each invoke result will be treated.
+	 * \tparam TFilter Type of provided filter objects
+	 * \details This class simply calls each provided filter successively. The provided algorithm will determine, how each invocation result will be treated.
+	 */
 	template <class TAlgorithm, class... TFilter>
 	class FilterChain
 	{
 	public:
+		/**
+		 * \brief Constructor
+		 * \param filter Filter objects
+		 */
 		constexpr explicit FilterChain(TFilter ...filter) :
 			m_Algorithm{},
 			m_Filter{ std::move(filter)... }
 		{
 		}
 
+		/**
+		 * \brief Constructor overload
+		 * \param algorithm Algorithm object
+		 * \param filter Filter objects
+		 */
 		constexpr explicit FilterChain(TAlgorithm algorithm, TFilter ...filter) :
 			m_Algorithm(std::move(algorithm)),
 			m_Filter{ std::forward<TFilter>(filter)... }
 		{
 		}
 
+		/**
+		 * \brief Call-operator
+		 * \tparam TRecord Concrete type of the used Record object
+		 * \param rec Record object
+		 * \return Returns true, if the Record should be handled. False if skipped.
+		 */
 		template <Record TRecord>
 		constexpr bool operator()(const TRecord& rec)
 		{
@@ -141,6 +189,10 @@ namespace sl::log
 	//template <std::predicate<const Record&>... TFilter>
 	//using FilterConjunction = FilterChain<FilterAnyOf, TFilter...>;
 
+	/**
+	 * \brief Convenience type for chaining multiple filter with AND
+	 * \tparam TFilter Type of provided filter objects
+	 */
 	template <class... TFilter>
 	class FilterAllOf :
 		public FilterChain<detail::TupleAnyOf, TFilter...>
@@ -148,12 +200,20 @@ namespace sl::log
 		using Algorithm = detail::TupleAnyOf;
 
 	public:
+		/**
+		 * \brief Constructor
+		 * \param filter Filter objects
+		 */
 		constexpr explicit FilterAllOf(TFilter ... filter) :
 			FilterChain<Algorithm, TFilter...>{ std::move(filter)... }
 		{
 		}
 	};
 
+	/**
+	 * \brief Convenience type for chaining multiple filter with OR
+	 * \tparam TFilter Type of provided filter objects
+	 */
 	template <class... TFilter>
 	class FilterAnyOf :
 		public FilterChain<detail::TupleAnyOf, TFilter...>
@@ -161,12 +221,20 @@ namespace sl::log
 		using Algorithm = detail::TupleAnyOf;
 
 	public:
+		/**
+		 * \brief Constructor
+		 * \param filter Filter objects
+		 */
 		constexpr explicit FilterAnyOf(TFilter ... filter) :
 			FilterChain<Algorithm, TFilter...>{ std::move(filter)... }
 		{
 		}
 	};
 
+	/**
+	 * \brief Convenience type for chaining multiple filter with NOR
+	 * \tparam TFilter Type of provided filter objects
+	 */
 	template <class... TFilter>
 	class FilterNoneOf :
 		public FilterChain<detail::TupleNoneOf, TFilter...>
@@ -174,35 +242,81 @@ namespace sl::log
 		using Algorithm = detail::TupleNoneOf;
 
 	public:
+		/**
+		 * \brief Constructor
+		 * \param filter Filter objects
+		 */
 		constexpr explicit FilterNoneOf(TFilter ... filter) :
 			FilterChain<Algorithm, TFilter...>{ std::move(filter)... }
 		{
 		}
 	};
 
+	/**
+	 * \brief Factory function for creating ProjectionFilter of Record::message member
+	 * \tparam TRecord Concrete Record type on which to apply the projection
+	 * \tparam TUnaryPredicate Invokable type, which has to accept objects the actual Record::Message_t type
+	 * \param predicate Predicate object
+	 * \return ProjectionFilter object
+	 * \details This is the preferable way creating a filter for the Record::message member, because the predicate becomes strong checked via
+	 * concept and therefore will provide much clearer feedback in cases of error, while creating ProjectionFilter objects manually will
+	 * potentially result in harder to read error message. 
+	 */
 	template <Record TRecord, std::predicate<const typename TRecord::Message_t&> TUnaryPredicate>
 	constexpr auto makeMessageFilterFor(TUnaryPredicate&& predicate)
 	{
 		return ProjectionFilter{ &TRecord::message, std::forward<TUnaryPredicate>(predicate) };
 	}
 
+	/**
+	 * \brief Factory function for creating ProjectionFilter of Record::severity member
+	 * \tparam TRecord Concrete Record type on which to apply the projection
+	 * \tparam TUnaryPredicate Invokable type, which has to accept objects the actual Record::SeverityLevel_t type
+	 * \param predicate Predicate object
+	 * \return ProjectionFilter object
+	 * \details This is the preferable way creating a filter for the Record::severity member, because the predicate becomes strong checked via
+	 * concept and therefore will provide much clearer feedback in cases of error, while creating ProjectionFilter objects manually will
+	 * potentially result in harder to read error message. 
+	 */
 	template <Record TRecord, std::predicate<const typename TRecord::SeverityLevel_t&> TUnaryPredicate>
 	constexpr auto makeSeverityFilterFor(TUnaryPredicate&& predicate)
 	{
 		return ProjectionFilter{ &TRecord::severity, std::forward<TUnaryPredicate>(predicate) };
 	}
 
+	/**
+	 * \brief Factory function for creating ProjectionFilter of Record::channel member
+	 * \tparam TRecord Concrete Record type on which to apply the projection
+	 * \tparam TUnaryPredicate Invokable type, which has to accept objects the actual Record::Channel_t type
+	 * \param predicate Predicate object
+	 * \return ProjectionFilter object
+	 * \details This is the preferable way creating a filter for the Record::channel member, because the predicate becomes strong checked via
+	 * concept and therefore will provide much clearer feedback in cases of error, while creating ProjectionFilter objects manually will
+	 * potentially result in harder to read error message. 
+	 */
 	template <Record TRecord, std::predicate<const typename TRecord::Channel_t&> TUnaryPredicate>
 	constexpr auto makeChannelFilterFor(TUnaryPredicate&& predicate)
 	{
 		return ProjectionFilter{ &TRecord::channel, std::forward<TUnaryPredicate>(predicate) };
 	}
 
+	/**
+	 * \brief Factory function for creating ProjectionFilter of Record::timePoint member
+	 * \tparam TRecord Concrete Record type on which to apply the projection
+	 * \tparam TUnaryPredicate Invokable type, which has to accept objects the actual Record::TimePoint_t type
+	 * \param predicate Predicate object
+	 * \return ProjectionFilter object
+	 * \details This is the preferable way creating a filter for the Record::timePoint member, because the predicate becomes strong checked via
+	 * concept and therefore will provide much clearer feedback in cases of error, while creating ProjectionFilter objects manually will
+	 * potentially result in harder to read error message. 
+	 */
 	template <Record TRecord, std::predicate<const typename TRecord::TimePoint_t&> TUnaryPredicate>
 	constexpr auto makeTimePointFilterFor(TUnaryPredicate&& predicate)
 	{
 		return ProjectionFilter{ &TRecord::timePoint, std::forward<TUnaryPredicate>(predicate) };
 	}
+
+	/** @}*/
 }
 
 #endif
