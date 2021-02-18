27d631553a53c3938128558828f782bae2071fba
DNKpp <DNKpp2011@gmail.com>
2021-02-19 00:36:18 +0100

    update FlushPolicies example
    Signed-off-by: DNKpp <DNKpp2011@gmail.com>

--- src/examples/FlushPolicies/main.cpp
+++ src/examples/FlushPolicies/main.cpp
@@ -17,6 +17,12 @@ inline std::unique_ptr<logging::Core_t> makeLoggingCore()
 	// create Sink in disabled state, thus we are able to configure it before it does handle any records
 	// will be automatically enabled when current scoped is left
 	auto consoleSink = core->makeDisabledSink<logging::BasicSink_t>(std::cout);
+
+	consoleSink->setFlushPolicy(
+		sl::log::makeSeverityFlushPolicyFor<logging::Record_t>(sl::log::Greater{ logging::SeverityLevel::error })		// performs flush if Record with error level has been handled.
+	);
+
+	// note: this will override the previous policy
 	consoleSink->setFlushPolicy(
 		sl::log::ByteCountFlushPolicy{ 100 }	// performs flush when the threshold of 100 bytes is exceeded
 	);
@@ -34,7 +40,7 @@ int main()
 
 	gLog() << "Print my important hint!";																		// flushes here
 
-	gLog() << "This" << " will " << " create " << " a " << " concatenated " << " warning " << " message";		// not flushed
+	gLog() << "This" << " will" << " create" << " a" << " concatenated" << " warning" << " message";			// not flushed
 
 	gLog() << "Back to info";																					// flushes again
 }
