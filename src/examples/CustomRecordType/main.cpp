64c36885099da5ac11c4bc1aa59a30b3747153a7
DNKpp <DNKpp2011@gmail.com>
2021-02-24 02:26:30 +0100

    add CustomRecordType and CustomizeBaseRecord examples
    Signed-off-by: DNKpp <DNKpp2011@gmail.com>

---
+++ src/examples/CustomRecordType/main.cpp
@@ -0,0 +1,85 @@
+//          Copyright Dominic Koepke 2021 - 2021.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          https://www.boost.org/LICENSE_1_0.txt)
+
+#include <Simple-Log/PresetTypes.hpp>
+#include <Simple-Log/Simple-Log.hpp>
+
+#include <iostream>
+#include <sstream>
+
+using namespace sl::log;
+using preset::SevLvl;
+
+class MyCustomRecord :
+	public preset::Record_t
+{
+public:
+	using Domain_t = std::string;
+	
+	Domain_t domain;
+};
+
+// Yes, these are a few typedefs which might seem tedious at the first glance, but you'll usually need to do this once per program.
+using Core_t = Core<MyCustomRecord>;
+using BasicSink_t = BasicSink<MyCustomRecord>;
+using FileSink_t = FileSink<MyCustomRecord>;
+using Logger_t = BaseLogger<MyCustomRecord>;
+
+// Now that our Record class contains an additional domain property, we would like to have a manipulator for our RecordBuilder
+using RecordBuilder_t = RecordBuilder<MyCustomRecord>;
+
+class SetDomain
+{
+public:
+	using Domain_t = MyCustomRecord::Domain_t;
+	
+	explicit SetDomain(Domain_t data) :
+		m_Data{ std::move(data) }
+	{
+	}
+
+	void operator ()(MyCustomRecord& rec)
+	{
+		rec.domain = std::move(m_Data);
+	}
+
+private:
+	Domain_t m_Data;
+};
+
+inline Core_t gCore;
+inline auto gLog = makeLogger<Logger_t>(gCore, SevLvl::info);
+inline auto& gConsoleSink
+{
+	[&]() -> auto&
+	{
+		// let's create the console sink in disabled state. Will become automatically enabled after this scope is left.
+		auto wrappedSink = gCore.makeDisabledSink<BasicSink_t>(std::cout);
+		// setting up a custom formatter, thus for each Record only the domain followed by the message will be printed.
+		wrappedSink->setFormatter(
+								[](const MyCustomRecord& record)
+								{
+									std::stringstream out;
+									out << record.domain << " >> " << record.message();
+									return std::move(out).str();
+								}
+								);
+
+		return *wrappedSink;
+	}()
+};
+
+int main()
+{
+	gLog() << "Hello, World!";
+	// this clearly belongs to the domain of palindromes, thus let's tag it like this!
+	gLog() << SetDomain("palindrome") << "A Man, A Plan, A Canal, Panama!";
+}
+
+/* The output of the console looks like this:
+ >> Hello, World!
+palindrome >> A Man, A Plan, A Canal, Panama!
+ 
+ */
---
+++ src/examples/CustomizeBaseRecord/main.cpp
@@ -0,0 +1,71 @@
+//          Copyright Dominic Koepke 2021 - 2021.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          https://www.boost.org/LICENSE_1_0.txt)
+
+#include <Simple-Log/PresetTypes.hpp>
+#include <Simple-Log/Simple-Log.hpp>
+
+#include <iostream>
+#include <sstream>
+
+using namespace sl::log;
+using preset::SevLvl;
+
+enum class Channel
+{
+	standard,
+	network,
+	stats
+};
+
+// A simple overload, thus our Channel enum can be printed into every ostream object.
+inline std::ostream& operator <<(std::ostream& out, Channel lvl)
+{
+	constexpr const char* str[] = { "standard", "network", "stats" };
+	out << str[static_cast<std::size_t>(lvl)];
+	return out;
+}
+
+// Yes, these are a few typedefs which might seem tedious at the first glance, but you'll usually need to do this once per program.
+using Record_t = BaseRecord<SevLvl, Channel>;
+using Core_t = Core<Record_t>;
+using BasicSink_t = BasicSink<Record_t>;
+using FileSink_t = FileSink<Record_t>;
+using Logger_t = BaseLogger<Record_t>;
+
+inline Core_t gCore;
+// every Record, which will be created by this Logger will be from Channel "standard" by default 
+inline auto gLog = makeLogger<Logger_t>(gCore, SevLvl::info, Channel::standard);
+inline auto& gConsoleSink
+{
+	[&]() -> auto&
+	{
+		// let's create the console sink in disabled state. Will become automatically enabled after this scope is left.
+		auto wrappedSink = gCore.makeDisabledSink<BasicSink_t>(std::cout);
+		// Only messages with the Channel network shall be printed onto the console
+		wrappedSink->setFilter(makeChannelFilterFor<Record_t>(Equals{ Channel::network }));
+		// setting up a custom formatter, thus for each Record only the channel followed by the message will be printed.
+		wrappedSink->setFormatter(
+								[](const Record_t& record)
+								{
+									std::stringstream out;
+									out << record.channel() << " >> " << record.message();
+									return std::move(out).str();
+								}
+								);
+
+		return *wrappedSink;
+	}()
+};
+
+int main()
+{
+	gLog() << "Hello, World!";	// this message is ignored by our Console sink
+	gLog() << SetChan(Channel::network) << "Hello, Network!"; // our Console sink handles this record.
+}
+
+/* The output of the console looks like this:
+ network >> Hello, Network!
+
+ */
